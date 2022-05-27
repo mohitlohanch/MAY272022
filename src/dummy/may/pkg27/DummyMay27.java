eb30ec8322d2f1d9d74d0050e93cfe7b02068444
Mohit Lohanch <lohanch@sheridancollege.ca>
2022-05-27 13:41:52 -0400

    added comments

--- src/dummy/may/pkg27/DummyMay27.java
+++ src/dummy/may/pkg27/DummyMay27.java
@@ -16,6 +16,7 @@ public class DummyMay27 {
     public static void main(String[] args) {
         // TODO code application logic here
         //comment added at 1.37pm
+        //comments added at 1.40pm
     }
     
 }
